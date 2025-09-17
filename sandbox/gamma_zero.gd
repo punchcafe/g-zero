0cc5603d3315c282e5bbfd6261bb46ee7194f8bf
Luca Fruzza <lfruzza@hotmail.co.uk>
2025-09-18 00:14:35 +0100

    miracle: somehow get the spherical motion working

--- sandbox/gamma_zero.gd
+++ sandbox/gamma_zero.gd
@@ -8,13 +8,10 @@ var _vector := SphereSurfaceVector.new()
 
 func _physics_process(delta: float) -> void:
 	var translation := Input.get_vector("legs_left", "legs_right", "legs_up", "legs_down")
-	_vector = _vector.translate(translation * 0.2)
-	var polar_axis := Vector3.UP.rotated(Vector3(1,0,0), _vector._coordinate.theta)
-	var other_axis := Vector3(1,0,0).rotated(Vector3(0,1,0), _vector._coordinate.fi)
+	_vector = _vector.translate(translation * 0.2)	
 	self.position = _vector.cartesian_position()
-	self.rotation = Vector3(0,0,0)
-	self.rotate(polar_axis, _vector._coordinate.fi)
-	self.rotate(other_axis, _vector._coordinate.theta)
+	
+	self.rotation = Vector3(0, _vector._coordinate.fi, -1 * _vector._coordinate.theta)
 
 func _process(delta) -> void:
 	pass
