0e99bb8097a0f052923ba5498735efa7f07a56c6
Luca Fruzza <lfruzza@hotmail.co.uk>
2025-09-17 23:28:09 +0100

    fix: fix SphericalCoordinate

--- sandbox/spherical_coordinate.gd
+++ sandbox/spherical_coordinate.gd
@@ -1,15 +1,22 @@
 extends RefCounted
 class_name SphericalCoordinate
 
-const normal_zenith := Vector3(0,1,0)
+const POLAR_AXIS := Vector3(0,1,0)
 
-var fi := 0.0
+# Polar angle: andle between this radial line and a given polar axis
 var theta := PI/2
+# azimuth angle: angle of rotation (counter-clock-wise) of the radial line around the polar axis.
+# 0 angle = Vector(1,0,0)
+var fi := 0.0
+# Distance from center
 var radius := 0.0
-var zenith := Vector3(0,1,0)
 
 func to_cartesian() -> Vector3:
+	# For equations, counter clockwise moves from positive x to positive z
+	# but in our scenario, counter clockwise moves to negative z, so we flip the
+	# polarity so that it can match godot rotation.
+	var inverted_fi := -1 * fi
 	return Vector3(\
-	radius * sin(theta) * cos(fi),\
-	radius * sin(theta) * sin(fi),\
-	radius * cos(theta))
+	radius * sin(theta) * cos(inverted_fi),\
+	radius * cos(theta),
+	radius * sin(theta) * sin(inverted_fi))
--- test/unit/test_spherical_coordinate.gd
+++ test/unit/test_spherical_coordinate.gd
@@ -5,15 +5,31 @@ func test_to_cartesian():
 		# Rotatate through theta while keeping fi 0
 		# TODO: determine the best orientation for this, as this is currently 
 		# following academinc with Z pointing up.
-		{"fi": 0, "theta": 0, "radius": 10, "expected": Vector3(0,0,10.0)},\
+		{"fi": 0, "theta": 0, "radius": 10, "expected": Vector3(0,10.0,0)},\
 		{"fi": 0, "theta": PI/2, "radius": 10, "expected": Vector3(10.0,0,0)},\
-		{"fi": 0, "theta": PI, "radius": 10, "expected": Vector3(0,0,-10.0)},\
+		{"fi": 0, "theta": PI, "radius": 10, "expected": Vector3(0,-10.0, 0.0)},\
 		{"fi": 0, "theta": 3*PI/2, "radius": 10, "expected": Vector3(-10.0,0,0)},\
-		# TODO: cases
-		# - Keep theta at zero and rotate through fi
-		# - Keep theta at PI and rotate through fi
-		# - different R values for different rotations
-		# - ...define more...
+		
+		{"fi": PI/2, "theta": 0, "radius": 10, "expected": Vector3(0,10.0,0)},\
+		{"fi": PI/2, "theta": PI/2, "radius": 10, "expected": Vector3(0,0,-10.0)},\
+		{"fi": PI/2, "theta": PI, "radius": 10, "expected": Vector3(0,-10.0, 0.0)},\
+		{"fi": PI/2, "theta": 3*PI/2, "radius": 10, "expected": Vector3(0,0,10.0)},\
+
+
+		{"fi": -1 * PI/2, "theta": 0, "radius": 10, "expected": Vector3(0,10.0,0)},\
+		{"fi": -1 * PI/2, "theta": PI/2, "radius": 10, "expected": Vector3(0,0,10.0)},\
+		{"fi": -1 * PI/2, "theta": PI, "radius": 10, "expected": Vector3(0,-10.0, 0.0)},\
+		{"fi": -1 * PI/2, "theta": 3*PI/2, "radius": 10, "expected": Vector3(0,0,-10.0)},\
+
+		{"fi": PI, "theta": 0, "radius": 10, "expected": Vector3(0,10.0,0)},\
+		{"fi": PI, "theta": PI/2, "radius": 10, "expected": Vector3(-10.0,0,0)},\
+		{"fi": PI, "theta": PI, "radius": 10, "expected": Vector3(0,-10.0, 0.0)},\
+		{"fi": PI, "theta": 3*PI/2, "radius": 10, "expected": Vector3(10.0,0,0)},\
+		
+		{"fi": 3 * PI/2, "theta": 0, "radius": 10, "expected": Vector3(0,10.0,0)},\
+		{"fi": 3 * PI/2, "theta": PI/2, "radius": 10, "expected": Vector3(0,0,10.0)},\
+		{"fi": 3 * PI/2, "theta": PI, "radius": 10, "expected": Vector3(0,-10.0, 0.0)},\
+		{"fi": 3 * PI/2, "theta": 3*PI/2, "radius": 10, "expected": Vector3(0,0,-10.0)},\
 	]
 	for case in cases:
 		var subject = SphericalCoordinate.new()
